4e6e282c6db21a66531ddd004eb19aea8a0bab64
kysongardner <kysongardner1@gmail.com>
2021-01-29 17:04:04 -0700

    Week 4 stuff

---
+++ src/js/productList.js
@@ -0,0 +1,35 @@
+import { renderListWithTemplate } from "./utils.js"
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category
+    this.dataSource = dataSource
+    this.listElement = listElement
+
+    
+  }
+  async init(){
+      const list = await this.dataSource.getProductsData()
+      this.renderList(list)
+  }
+  renderList(list) {
+    this.listElement.innerHTML = ""
+    const template = document.getElementById("product-card-template");
+    const filterList = this.filterProducts(list)
+    renderListWithTemplate(template, this.listElement, filterList, this.prepareTemplate)
+    }
+  
+  prepareTemplate(templateClone, product){
+    templateClone.querySelector(".card__img").setAttribute("src", product.Image)
+    templateClone.querySelector(".card__brand").innerHTML = product.Brand.Name
+    templateClone.querySelector(".card__name").innerHTML = product.NameWithoutBrand
+    templateClone.querySelector(".product-card__price").innerHTML = product.ListPrice
+    return templateClone
+  
+  }
+  filterProducts(list) {
+    const tents = ["880RR", "985RF", "985PR", "344YJ"]
+    return list.filter(item => tents.indexOf(item.Id) >= 0)
+
+  }
+}
