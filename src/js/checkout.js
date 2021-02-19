4e4fd7a2eb692f17e3781030804eba4619b7d2c5
01ddgodwin <ddgodwin01@byui.edu>
2021-02-19 12:16:13 -0700

    Checkout process

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,11 @@
 import { loadHeaderFooter } from "./utils.js";
+import CheckoutProcess from "./CheckoutProcess.js";
 
 loadHeaderFooter();
+
+
+const checkout = new CheckoutProcess("so-cart")
+checkout.calcAndDisplaySubtotal()
+checkout.calcAndDisplayShippingTaxOrderTotal()
+
+
