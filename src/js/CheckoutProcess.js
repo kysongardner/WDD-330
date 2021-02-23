c5910bdd38902b5a4006676272e55aa9db8341e2
01ddgodwin <ddgodwin01@byui.edu>
2021-02-23 10:01:51 -0700

    Removed old code

--- src/js/CheckoutProcess.js
+++ src/js/CheckoutProcess.js
@@ -3,59 +3,6 @@ import ExternalServices from "./ExternalServices.js";
 
 const services = new ExternalServices();
 
-// export default class ChekcoutProcess {
-//     constructor(key, outputSelector){
-//         this.key = key
-//         this.itemTotal = 0;
-//         this.list = getLocalStorage(this.key);
-//         this.subtotalPrice = 0
-//         this.shippingEstimate = document.querySelector("#shipping-estimate").value
-//         this.tax = document.querySelector("#tax").value
-//         this.orderTotal = document.querySelector("#order-total").value
-//     }
-
-//     calcAndDisplaySubtotal(){
-//       // console.log(this.list)
-//       // for (let index = 0; index < this.list.length; index++) {
-//       //   console.log(this.list[index].FinalPrice)
-//       //   this.subtotalPrice += this.list[index].FinalPrice.value
-
-//       //   let subtotal = this.subtotalPrice
-//       //   subtotal.textContent = subtotal + "(" + index + ")" + this.subtotalPrice
-
-//       const summaryElement = document.querySelector(this.outputSelector + " #cartTotal");
-//       const itemNumElement = document.querySelector(this.outputSelector + " #num-items");
-//       itemNumElement.innerText = getLocalStorage(this.key).length;
-//       // calculate the total of all the items in the cart
-//       const amounts = this.list.map((item) => item.FinalPrice);
-//         this.subtotalPrice = amounts.reduce((sum, item) => sum + item);
-//         summaryElement.innerText = "$" + this.itemTotal;
-//       }
-
-//     calcAndDisplayShippingTaxOrderTotal(listOfData){
-//         // use this.shippingEstimate
-//         // this.tax
-//         // this.orderTotal
-
-//         // How do we get the data here as a parameter???
-//         // get object data as a parameter
-//         // iterate through it . . .
-//         // if items == 1 then shipping = $10
-//         // for each item shipping += 2
-//         // for each item tax = 0.06
-//         // for each item subtotal = item value + shipping * tax
-//     }
-
-//     callCheckout(formData){
-//       const formInfo = new FormData(formData)
-//       const converted = Object.fromEntries(formInfo.entries())
-//       console.log(converted)
-//       eS.checkout()
-
-//     }
-
-// }
-
 function formDataToJSON(formElement) {
   var formData = new FormData(formElement),
     convertedJSON = {};
@@ -98,7 +45,6 @@ export default class CheckoutProcess {
     const summaryElement = document.querySelector("#cartTotal");
     const itemNumElement = document.querySelector("#num-items");
     itemNumElement.innerText = this.list.length;
-    // calculate the total of all the items in the cart
     const amounts = this.list.map((item) => item.FinalPrice);
     this.itemTotal = amounts.reduce((sum, item) => sum + item);
     summaryElement.innerText = "$" + this.itemTotal;
