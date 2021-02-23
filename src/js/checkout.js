d73e1cfd7e9cf6e3cf6d9afb5df3afff3bd06efa
kysongardner <kysongardner1@gmail.com>
2021-02-23 10:46:07 -0700

    fixed validation

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -13,7 +13,10 @@ document
   .addEventListener("blur", checkout.calculateOrdertotal.bind(checkout));
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
-
+  var myForm = document.forms[0];
+  var chk_status = myForm.checkValidity();
+  myForm.reportValidity();
+  if(chk_status)
   checkout.checkout();
 });
 
